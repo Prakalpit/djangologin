f6d46f511420459309698a77c387b5f1cb47dbcc
Prakalpit <prakalpit.sangram@gmail.com>
2020-12-20 14:10:39 +0545

    signup page has been added(screenshots are available).

--- src/urls.py
+++ src/urls.py
@@ -19,5 +19,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('gazals/', TemplateView.as_view(template_name='gazals.html'), name='Gazals'),
+    path('accounts/', include('accounts.urls')), # new
 ]
